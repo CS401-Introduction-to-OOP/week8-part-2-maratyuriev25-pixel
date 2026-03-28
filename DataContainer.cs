98d6f3ca4ec82092194caaca8bf984fa54c51f76
maratyuriev25-pixel <marat.yuriev.25@kse.org.ua>
2026-03-28 18:01:51 +0200

    Step2 done

---
+++ DataContainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class DataContainer<T> where T : class
+{
+    private readonly List<T> _items = new();
+
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+    }
+    public long GetTotalSize()
+    {
+        long result = 0;
+        foreach (var item in _items) {
+            result += item.GetTotalSize();
+    }
+}
